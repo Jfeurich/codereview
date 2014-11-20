5c1a45a0af966007328763b57b5af46b355ea742
Gorgony <n.j.vannispen@gmail.com>
2014-11-20 12:33:48 +0100

    Hoi

--- src/nl/hu/tho4/Main.java
+++ src/nl/hu/tho4/Main.java
@@ -3,7 +3,7 @@ package nl.hu.tho4;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Nathan is een sukkel!");
+		System.out.println("Iedereen is een sukkel!");
 		System.out.println("OHAI");
 
 	}
