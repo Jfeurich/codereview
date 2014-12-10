647f6a0e34b51bacde706a8be6e64f804b5c9c06
Jfeurich <jayh4x0r@gmail.com>
2014-12-10 15:39:22 +0100

    Rulebuilder 3: The rulebuildening

--- src/nl/hu/tho4/domain/BusinessRuleBuilder.java
+++ src/nl/hu/tho4/domain/BusinessRuleBuilder.java
@@ -12,19 +12,30 @@ public class BusinessRuleBuilder {
 		rule.setBusinessruletype(businessruletype);
 		return rule;
 	}
-	public BusinessRule attRangeRule(String ruleNaam, String error, Operator o,String errorType, Attribute a, Value v1, Value v2) {
+	public BusinessRule attRangeRule(String ruleNaam, String error, Operator o,String errorType, Attribute a, Value v1, Value v2,BusinessRuleType businessruletype) {
+		BusinessRule rule = new BusinessRule();
+		rule.setRuleNaam(ruleNaam);
+		rule.setError(error);
+		rule.setOperator(o);
+		rule.setErrorType(errorType);
+		rule.addAttribute(a);
+		rule.addValue(v1);
+		rule.addValue(v2);
+		rule.setBusinessruletype(businessruletype);
 		return rule;
 	}
-	public BusinessRule attListRule(String ruleNaam, String error, Operator o ,String  errorType, Attribute a) {
+	/*
+	public BusinessRule attListRule(String ruleNaam, String error, Operator o ,String  errorType, Attribute a,BusinessRuleType businessruletype) {
 		return rule;
 	}
-	public BusinessRule tupleCompareRule(String ruleNaam,String  error, Operator o ,String errorType, Attribute a1, Attribute a2){
+	public BusinessRule tupleCompareRule(String ruleNaam,String  error, Operator o ,String errorType, Attribute a1, Attribute a2,BusinessRuleType businessruletype){
 		return rule;
 	}
-	public BusinessRule interEntityCompareRule(String ruleNaam,String error,Operator o, String errorType, Attribute a1, Attribute a2){
+	public BusinessRule interEntityCompareRule(String ruleNaam,String error,Operator o, String errorType, Attribute a1, Attribute a2,BusinessRuleType businessruletype){
 		return rule;
 	}
-	public BusinessRule entityOtherRule(String ruleNaam, String error, Operator o, String errorType, Attribute a, String s){
+	public BusinessRule entityOtherRule(String ruleNaam, String error, Operator o, String errorType, Attribute a, String s,BusinessRuleType businessruletype){
 		return rule;
 	}
+	*/
 }
