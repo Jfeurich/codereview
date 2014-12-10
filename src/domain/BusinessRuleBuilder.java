832f398038b1b38fa3b1bd99e3c334d64be04f81
Jfeurich <jayh4x0r@gmail.com>
2014-12-10 13:48:54 +0100

    asd

--- src/domain/BusinessRuleBuilder.java
+++ src/domain/BusinessRuleBuilder.java
@@ -2,7 +2,15 @@ package domain;
 
 public class BusinessRuleBuilder {
 	public BusinessRule attCompareRule(String ruleNaam,String error,Operator o, String errorType,Attribute a,Attribute a2) {
-		return null;
+		BusinessRule rule = new BusinessRule();
+		rule.setRuleNaam(ruleNaam);
+		rule.setError(error);
+		rule.setOperator(o);
+		rule.setErrorType(errorType);
+		rule.addAttribute(a);
+		rule.addAttribute(a2);
+		rule.setBusinessruletype(businessruletype);
+		return rule;
 	}
 	public BusinessRule attRangeRule(String ruleNaam, String error, Operator o,String errorType, Attribute a, Value v1, Value v2) {
 		return null;
