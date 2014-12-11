b929ec233a08699ce3d12fc63362acb1572ced0a
zaans2 <liam.h_indahouse@hotmail.com>
2014-12-11 13:47:34 +0100

    Kleine Update

--- src/nl/hu/tho4/domain/Generator.java
+++ src/nl/hu/tho4/domain/Generator.java
@@ -1,14 +1,14 @@
 package nl.hu.tho4.domain;
 
-import nl.hu.tho4.domain.template.Language;
-
 import java.util.ArrayList;
+
+import nl.hu.tho4.domain.template.Language;
 
 public class Generator {
 	private ArrayList<Language> languages = new ArrayList<>();
 
 	public void Generate(BusinessRule rule, Language l) {
-
+		
 	}
 
 	public void getBusinessRules() {
