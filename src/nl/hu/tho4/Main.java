ab267c75f58c8588d38de954d6f9733f1afe2f5f
Jfeurich <jayh4x0r@gmail.com>
2014-12-17 13:06:56 +0100

    foutmeldingen tijdelijk opgeeft

--- src/nl/hu/tho4/Main.java
+++ src/nl/hu/tho4/Main.java
@@ -4,14 +4,13 @@ import nl.hu.tho4.domain.businessrule.Attribute;
 import nl.hu.tho4.domain.businessrule.BusinessRule;
 import nl.hu.tho4.domain.Generator;
 import nl.hu.tho4.domain.businessrule.Value;
-import nl.hu.tho4.domain.businessrules.attCompareBusinessRule;
-import nl.hu.tho4.domain.businessrules.attRangeBusinessRule;
 import nl.hu.tho4.domain.template.Template;
 import nl.hu.tho4.domain.template.impl.CompRulePlSqlTemplate;
 
+@SuppressWarnings("unused")
 public class Main {
 	public static void main(String[] args) {
-		Value v = new Value();
+		/*Value v = new Value();
 		v.setWaardeNaam("ugh");
 		v.setValue("2");
 		v.setValueType("int");
@@ -19,15 +18,15 @@ public class Main {
 		v2.setWaardeNaam("ugh2");
 		v2.setValue("4");
 		v2.setValueType("int");
-		Attribute a = new Attribute("Ietsuitdedatabase","uitschema1","kolom1","tabel1");
-		Attribute a2 = new Attribute("Nogietsuitdedatabase","uitschema1","kolom1","tabel1");
-		BusinessRule rule = new attCompareBusinessRule(a,a2);
+		Attribute a = new Attribute("Ietsuitdedatabase","uitschema1","kolom1","tabel1", null);
+		Attribute a2 = new Attribute("Nogietsuitdedatabase","uitschema1","kolom1","tabel1", null);
+		BusinessRule rule = new BusinessRule(a,a2);
 		rule.setRuleNaam("regel1");
 		rule.setError("dit is een error");
 		rule.setErrorType("Custom");
 		Template t = new CompRulePlSqlTemplate();
 		Generator generator = new Generator();
-		generator.Generate(rule, t);
+		generator.Generate(rule, t);*/
 		// TODO Auto-generated method stub
 
 	}
