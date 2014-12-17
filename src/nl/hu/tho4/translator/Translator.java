6593d97e0d13ac07a8ea8455968b2351f3f5be56
Jfeurich <jayh4x0r@gmail.com>
2014-12-17 17:11:13 +0100

    even more generator code

--- src/nl/hu/tho4/translator/Translator.java
+++ src/nl/hu/tho4/translator/Translator.java
@@ -4,20 +4,53 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+import nl.hu.tho4.domain.businessrule.BusinessRule;
+
+import org.stringtemplate.v4.*;
+
 public class Translator {
 	private String language;
-	private Object businessrule;
-	
-	public final String Translator(String language) {
-		String languagetemplate = null;
-		switch (language){
-		case "plsql" : try {
-				languagetemplate = readFileToString("Stringtemplates/plsql.st");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}break;
+	private BusinessRule businessrule;
+	public String Translator(String language, BusinessRule businessrule){
+		ST query = new ST(Translatorlanguage(language));
+		query.add("$trigger_name$",generateTriggerName());
+		query.add("$table_name$",businessrule.getAttribute1().getTabel());
+		for(int i = 0;i<4;i++){
+			query.add("$value_declarations$","declareval");
 		}
+		query.add("$trigger_code$",generateTriggerCode());
+		query.add("$exception_code$",generateExceptionCode());
+		return query.render();
+	}
+	
+	private String generateTriggerName(){
+		String s ="";
+		return s;
+	}
+	
+	private String generateTriggerCode(){
+		String s ="";
+		return s;
+	}
+	
+	private String generateExceptionCode(){
+		String s ="";
+		return s;
+	}
+	
+	private final String Translatorlanguage(String language) {
+		String languagetemplate = null;
+		try{
+		switch (language){
+			case "plsql" : languagetemplate = readFileToString("Stringtemplates/plsql.st");break;
+			case "mysql" : languagetemplate = readFileToString("Stringtemplates/mysql.st");break;
+			}
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return languagetemplate;
 	}
 	
@@ -46,11 +79,11 @@ public class Translator {
 		this.language = language;
 	}
 
-	public Object getBusinessrule() {
+	public BusinessRule getBusinessrule() {
 		return businessrule;
 	}
 
-	public void setBusinessrule(Object businessrule) {
+	public void setBusinessrule(BusinessRule businessrule) {
 		this.businessrule = businessrule;
 	}
 
