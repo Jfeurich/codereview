94a06717a3670512bd34deca8858abc784a14754
Jfeurich <jayh4x0r@gmail.com>
2015-01-18 20:49:46 +0100

    This needs to be tested for nullpointerexceptions

--- src/main/java/nl/hu/tho6/controller/BusinessruleOphaalServlet.java
+++ src/main/java/nl/hu/tho6/controller/BusinessruleOphaalServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.ArrayList;
 
 public class BusinessruleOphaalServlet extends HttpServlet {
@@ -19,27 +20,29 @@ public class BusinessruleOphaalServlet extends HttpServlet {
     private int aantalBusinessRules = 0;
     private Generator generator = Generator.getInstance();
 
-//
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-//       TODO hier een arraylist neerzetten waar de businessrules in komen
-//        ConnectDBBusinessRule.getOngegenereerdeBusinessRules();
-//        if(ongeGenereerdeBusinessRule.size() == 0){
-//            returnMessage = "Er zijn geen businessrules te genereren.";
-//        } else {
-//            for(int i = 0; i < ongeGenereerdeBusinessRule.size(); i++){
-//                ST gegenereerdeBusinessRule = generator.generate(language, ongeGenereerdeBusinessRule.get(i));
-//                ConnectDBBusinessRule.saveBusinessrule(gegenereerdeBusinessRule);
-//                aantalBusinessRules++;
-//            }
-//            if(aantalBusinessRules == 1){
-//                returnMessage = "er is " + aantalBusinessRules + " businessrule gegenereerd.";
-//            } else {
-//                returnMessage = "er zijn " + aantalBusinessRules + " businessrules gegenereerd.";
-//            }
-//        }
-//
-//        String session = req.getParameter("SESSION");
-//        resp.sendRedirect("https://ondora01.hu.nl:8080/apex/f?p=2298:1:" + session + "::::P1_TEST:" + returnMessage);
-//    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        Connection con = null; // TODO waarde inzetten van database zonder dat deze in github verschijnt
+        ConnectDBBusinessRule cdbbr = new ConnectDBBusinessRule(con);
+        ongeGenereerdeBusinessRule = cdbbr.getOngegenereerdeBusinessRules();
+        if(ongeGenereerdeBusinessRule.size() == 0){
+            returnMessage = "Er zijn geen businessrules te genereren.";
+        } else {
+            for(int i = 0; i < ongeGenereerdeBusinessRule.size(); i++){
+                ST gegenereerdeBusinessRule = generator.generate(language, ongeGenereerdeBusinessRule.get(i));
+                // TODO dit aanpassen zodat het werkt
+                //ConnectDBBusinessRule.saveBusinessrule(gegenereerdeBusinessRule);
+                aantalBusinessRules++;
+            }
+            if(aantalBusinessRules == 1){
+                returnMessage = "er is " + aantalBusinessRules + " businessrule gegenereerd.";
+            } else {
+                returnMessage = "er zijn " + aantalBusinessRules + " businessrules gegenereerd.";
+            }
+        }
+
+        String session = req.getParameter("SESSION");
+        resp.sendRedirect("https://ondora01.hu.nl:8080/apex/f?p=2298:1:" + session + "::::P1_TEST:" + returnMessage);
+    }
 
 }
