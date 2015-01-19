48a8f6dac98d11677b52f7939110c43a1bbf8d8a
Jfeurich <jayh4x0r@gmail.com>
2015-01-19 13:02:54 +0100

    merge probleem ongedaan maken

--- src/main/java/nl/hu/tho6/persistence/connection/ConnectionFactory.java
+++ src/main/java/nl/hu/tho6/persistence/connection/ConnectionFactory.java
@@ -1,15 +1,14 @@
 package nl.hu.tho6.persistence.connection;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private static      ConnectionFactory instance     = new ConnectionFactory();
-    public static final String            URL          = "jdbc:oracle:thin/ondora01.hu.nl:8521/THO6";
-    public static final String            USER         = "tho6_2014_2b_team3";
-    public static final String            PASSWORD     = " tho6_2014_2b_team3";
-    public static final String            DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
+    private static ConnectionFactory instance = new ConnectionFactory();
+    public static final String URL = "jdbc:oracle:thin/ondora01.hu.nl:8521/THO6";
+    public static final String USER = "tho6_2014_2b_team3";
+    public static final String PASSWORD = " tho6_2014_2b_team3";
+    public static final String DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
 
     private ConnectionFactory() {
         try {
@@ -19,7 +18,7 @@ public class ConnectionFactory {
         }
     }
 
-    private Connection createConnection() {
+    public Connection createConnection() {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
